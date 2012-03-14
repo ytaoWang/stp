109147a3b80f998a8981d3e05ba820c20de033eb
ytaowang <yt0907.Wang@gmail.com>
2012-03-14 14:04:26 +0800

    subtree partition progject

---
+++ include/bitmap.h
@@ -0,0 +1,6 @@
+#ifndef __BITMAP_H__
+#define __BITMAP_H__
+
+
+
+#endif
---
+++ include/stp_types.h
@@ -0,0 +1,18 @@
+#ifndef __STP_TYPE_H__
+#define __STP_TYPE_H__
+
+#include <stdint.h>
+
+typedef uint32_t u32;
+typedef uint8_t u8;
+typedef uint64_t u64;
+
+typedef int32_t s32;
+typedef int8_t s8;
+typedef int64_t s64;
+
+
+typedef unsigned long ptr_t;
+
+
+#endif
